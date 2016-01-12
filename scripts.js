46525be630ab3d4b6becaf7437a7c0a2ed140d03
Tree Casiano <tree.pdx@gmail.com>
2016-01-11 20:52:59 -0800

    Add new quotes.

--- scripts.js
+++ scripts.js
@@ -16,7 +16,23 @@
     "When I'm stuck for a closing to a lyric, I will drag out my last resort:" +
     " overwhelming illogic.",
     "When I heard Little Richard, I mean, it just set my world on fire.",
-  "The humanists' replacement for religion: work really hard and somehow you'll either save yourself or you'll be immortal. Of course, that's a total joke, and our progress is nothing. There may be progress in technology but there's no ethical progress whatsoever."
+  "The humanists' replacement for religion: work really hard and somehow you'll either save yourself or you'll be immortal. Of course, that's a total joke, and our progress is nothing. There may be progress in technology but there's no ethical progress whatsoever.",
+    "Tomorrow belongs to those who can hear it coming.",
+    "I'm always amazed that people take what I say seriously. I don't even take what I say seriously.",
+    "Confront a corpse at least once. The absolute absence of life is the most disturbing and challenging confrontation you will ever have.",
+    "I can ask for cigarettes in every language.",
+    "I'm afraid of Americans.",
+    "There's a terror in knowing what the world is about.",
+    "Fame can take interesting men and thrust mediocrity upon them.",
+    "As you get older, the questions come down to about two or three. How long? And what do I do with the time I've got left?",
+    "Don't you love the Oxford Dictionary? When I first read it, I thought it was a really really long poem about everything.",
+    "I find only freedom in the realms of eccentricity...",
+    "The greatest thing you'll ever learn is just to love and be loved in return.",
+    "Turn and face the strange.",
+    "I'm a born librarian with a sex drive.",
+    "You would think that a rock star being married to a super-model would be one of the greatest things. It is.",
+    "I reinvented my image so many times that I'm in denial that I was originally an overweight Korean woman.",
+    "Talking about art is like dancing about architecture."
   ];
 
 var tempQuotesList = quotes.slice(0);
